939a596e6419138af9cf40331b22de076fd1f1fb
yichen <1097179511@qq.com>
2023-03-03 08:25:01 +0800

    version update
    update version

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def show_Adapters():
 async def main():
     f = Figlet(font="slant", width=100)
     print(f.renderText("BLE Security Tool"))
-    print(" Author: yichen               Version: 0.04\n")
+    print(" Author: yichen               Version: 1.00\n")
     meun = PrettyTable(["选项", "说明"])
     meun.align["选项"] = 'l'
     meun.align["说明"] = 'l'
